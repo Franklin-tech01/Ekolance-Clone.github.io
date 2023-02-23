c3288b153571250f36fb4977802809e24594c277
Franklin-tech01 <franklinmike2021@gmail.com>
2023-02-23 23:12:45 +0000

    commented js code needs to write new one

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-const hams = document.querySelector(".ham")
-const menu = document.querySelector(".nav__link")
+// const hams = document.querySelector(".ham")
+// const menu = document.querySelector(".nav__link")
 
-hams.addEventListener("click",() => {
-    hams.classList.toggle("active");
-    menu.classList.toggle("active");
-})
+// hams.addEventListener("click",() => {
+//     hams.classList.toggle("active");
+//     menu.classList.toggle("active");
+// })
 
-document.querySelector(".link").forEach(n => n.addEventListener("click", () => {
-    hams.classList.remove("active")
-    menu.classList.remove("active")
-}))
+// document.querySelector(".link").forEach(n => n.addEventListener("click", () => {
+//     hams.classList.remove("active")
+//     menu.classList.remove("active")
+// }))
